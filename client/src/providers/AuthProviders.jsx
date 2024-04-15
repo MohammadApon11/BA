505ae13307074c4c06612e6a0be22189c2d97790
moahammadApon <mohammadapon11.com>
2024-04-16 00:12:16 +0600

    auth prodiver loading state change

--- client/src/providers/AuthProviders.jsx
+++ client/src/providers/AuthProviders.jsx
@@ -19,7 +19,7 @@ const auth = getAuth(app);
 
 const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
-  const [loading, setLoading] = useState(false);
+  const [loading, setLoading] = useState(true);
   const googleProvider = new GoogleAuthProvider();
 
   const createUser = (email, password) => {
