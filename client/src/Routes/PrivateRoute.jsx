5a29d38efb156510d05e39fb71ae5028866725f0
moahammadApon <mohammadapon11.com>
2024-04-16 00:04:59 +0600

    firebase creadintial chagned

--- client/src/Routes/PrivateRoute.jsx
+++ client/src/Routes/PrivateRoute.jsx
@@ -4,17 +4,17 @@ import { Navigate, useLocation } from "react-router-dom";
 import Loader from "../Pages/Shared/Loader/Loader";
 
 const PrivateRoute = ({ children }) => {
-    const { user, loading } = useContext(AuthContext);
-    const location = useLocation()
-    if (loading) {
-        return <Loader />
-    }
+  const { user, loading } = useContext(AuthContext);
+  const location = useLocation();
+  if (loading) {
+    return <Loader />;
+  }
 
-    if (user) {
-        return children;
-    }
+  if (user) {
+    return children;
+  }
 
-    return <Navigate to="/login" state={{ from: location }} replace></Navigate>;
+  return <Navigate to="/login" state={{ from: location }} replace></Navigate>;
 };
 
 export default PrivateRoute;
